4c43d1e6a0d8f1bf445e232d011c2dc4f983e5f8
Casimir Crystal <casimir.crystal.blue@outlook.com>
2021-06-13 18:18:04 +0800

    fix: remove content for testing

---
+++ Game/game/controller/ProbsInit.js
@@ -0,0 +1,10 @@
+function generateExam() {
+    var probs = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        probs[_i - 0] = arguments[_i];
+    }
+}
+function probsInit() {
+    return generateExam(1213, 2559);
+}
+//# sourceMappingURL=ProbsInit.js.map
